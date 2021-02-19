dc393e857f9fcdbc197f16d8940c1c2f6c81dc1e
test2451 <79050952+test2451@users.noreply.github.com>
2021-02-19 23:10:27 +0530

    Update index.tsx

--- src/pages/RemoveLiquidity/index.tsx
+++ src/pages/RemoveLiquidity/index.tsx
@@ -3,8 +3,8 @@ import styled, { ThemeContext } from 'styled-components'
 import { splitSignature } from '@ethersproject/bytes'
 import { Contract } from '@ethersproject/contracts'
 import { TransactionResponse } from '@ethersproject/providers'
-import { Currency, currencyEquals, ETHER, Percent, WETH } from '@pancakeswap-libs/sdk'
-import { Button, Flex, Text } from '@pancakeswap-libs/uikit'
+import { Currency, currencyEquals, ETHER, Percent, WETH } from '@pieswap-libs/sdk'
+import { Button, Flex, Text } from '@pieswap-libs/uikit'
 import { ArrowDown, Plus } from 'react-feather'
 import { RouteComponentProps } from 'react-router'
 
@@ -123,7 +123,7 @@ export default function RemoveLiquidity({
       { name: 'verifyingContract', type: 'address' },
     ]
     const domain = {
-      name: 'Pancake LPs',
+      name: 'Pie LPs',
       version: '1',
       chainId,
       verifyingContract: pair.liquidityToken.address,
@@ -542,7 +542,7 @@ export default function RemoveLiquidity({
                                 currencyB === ETHER ? WETH[chainId].address : currencyIdB
                               }`}
                             >
-                              Receive WBNB
+                              Receive WOKT
                             </StyledInternalLink>
                           ) : oneCurrencyIsWETH ? (
                             <StyledInternalLink
@@ -550,7 +550,7 @@ export default function RemoveLiquidity({
                                 currencyA && currencyEquals(currencyA, WETH[chainId]) ? 'ETH' : currencyIdA
                               }/${currencyB && currencyEquals(currencyB, WETH[chainId]) ? 'ETH' : currencyIdB}`}
                             >
-                              Receive BNB
+                              Receive OKT
                             </StyledInternalLink>
                           ) : null}
                         </RowBetween>
