99fd038ba54eaa667f404df2d2dd2b14871e0ce2
test2451 <79050952+test2451@users.noreply.github.com>
2021-02-20 02:57:12 +0530

    Update index.tsx

--- src/components/CurrencyInputPanel/index.tsx
+++ src/components/CurrencyInputPanel/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useCallback } from 'react'
-import { Currency, Pair } from '@pancakeswap-libs/sdk'
-import { Button, ChevronDownIcon, Text } from '@pancakeswap-libs/uikit'
+import { Currency, Pair } from '@pieswap-libs/sdk'
+import { Button, ChevronDownIcon, Text } from '@pieswap-libs/uikit'
 import styled from 'styled-components'
 import { darken } from 'polished'
 import { useCurrencyBalance } from '../../state/wallet/hooks'
