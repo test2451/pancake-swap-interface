bb984ce1e6e26d65f57663d45777ccac9b9e3e92
test2451 <79050952+test2451@users.noreply.github.com>
2021-02-19 23:25:37 +0530

    Update PoolPriceBar.tsx

--- src/pages/AddLiquidity/PoolPriceBar.tsx
+++ src/pages/AddLiquidity/PoolPriceBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Currency, Percent, Price } from '@pancakeswap-libs/sdk'
-import { Text } from '@pancakeswap-libs/uikit'
+import { Currency, Percent, Price } from '@pieswap-libs/sdk'
+import { Text } from '@pieswap-libs/uikit'
 import { AutoColumn } from '../../components/Column'
 import { AutoRow } from '../../components/Row'
 import { ONE_BIPS } from '../../constants'
